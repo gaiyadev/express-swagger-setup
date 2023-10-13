9ae0bd41b2b2280b55a7522ba712f0bb4f5ed4b4
gaiyaobed <95748043+gaiyaobed@users.noreply.github.com>
2023-10-14 00:10:49 +0100

    dd

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ const swaggerUi = require('swagger-ui-express');
 const swaggerDocument = require('./../swagger.json'); // Load your Swagger JSON file
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT;
 
 app.use(bodyParser.json());
 
